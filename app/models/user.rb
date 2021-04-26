acece4c952a5df10807b76891d0ba7b08d78bf62
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 18:13:59 +0900

    update: has_secure_password忘れてたので追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum:255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
   validates :password, length: {minimum: 6 }
   
+  has_secure_password
   # mount_uploader :image, ImageUploader
   
 end
