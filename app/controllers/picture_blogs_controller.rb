0fdca7a6f5e1600ef9ca4b8174a871c75922ea3c
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-27 00:07:57 +0900

    update: CRUD機能作成（ページ作成）

--- app/controllers/picture_blogs_controller.rb
+++ app/controllers/picture_blogs_controller.rb
@@ -1,16 +1,60 @@
 class PictureBlogsController < ApplicationController
+
   def new
+    if params[:back]
+      @picture_blog = PictureBlog.new(picture_blog_params)
+    else
+      @picture_blog = PictureBlog.new
+    end
+  end
+
+  def create
+    @picture_blog = PictureBlog.new(picture_blog_params)
+    @picture_blog.user_id = current_user.id
+    if @picture_blog.save
+      redirect_to picture_blogs_path, notice: "投稿できました！"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @picture_blog.update(picture_blog_params)
+      redirect_to picture_blogs_path, notice: "投稿できました！"
+    else
+      render :new
+    end
   end
 
   def index
+    @picture_blog = PictureBlog.all
   end
 
   def show
   end
 
   def edit
+    redirect_to picture_blogs_path, notice: "不正操作を記録しました。" unless current_user.id == @picture_blog.id
   end
 
   def confirm
+    @picture_blog = PictureBlog.new(picture_blog_params)
+    @picture_blog.user_id = current_user.id
+    render :new if @picture_blog.invalid?
+  end
+
+  def destroy
+    @picture_blog.destroy
+    redirect_to picture_blogs_path, notice: "削除できました！"
+  end
+
+  private
+
+  def picture_blog_params
+    params.require(:picture_blog).permit(:title, :content, :image, :image_cache)
+  end
+
+  def set_picture_blog
+    @picture_blog = PictureBlog.find(params[:id])
   end
 end
