31242829ba259bde0552f402a1247a5c5613ab05
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 13:33:00 +0900

    update: controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'favorites/index'
+  get 'sessions/new'
+  get 'picture_blogs/new'
+  get 'picture_blogs/index'
+  get 'picture_blogs/show'
+  get 'picture_blogs/edit'
+  get 'picture_blogs/confirm'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
