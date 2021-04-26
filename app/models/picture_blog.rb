fd5f2cc64e00b97147c656a65cc100284082d1f6
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 13:46:46 +0900

    update: アソシエーション追加

--- app/models/picture_blog.rb
+++ app/models/picture_blog.rb
@@ -1,3 +1,4 @@
 class PictureBlog < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :picture_blogs
+  has_many :favorites, dependent: :destroy
 end
