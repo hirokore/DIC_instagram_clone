32c7bd61292f1d88280812a84892ed6d14a6a0c3
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 14:40:21 +0900

    update: ユーザー新規登録画面追加(エラーが発生するためuploader設定はコメントアウト)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :picture_blogs
   has_many :favorites, dependent: :destroy
 
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
   
 end
