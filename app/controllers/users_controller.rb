c86f57cba265de7d8914e44c0ddad2ec77b5e199
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-27 03:13:51 +0900

    update: ユーザーページ画像表示機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,20 +15,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "投稿できました！"
+    else
+      render :edit
+    end
+  end
+
   def show
     @users = User.find(params[:id])
     # redirect_to picture_blogs_path, notice: "不正操作を記録しました。" unless current_user.id == @users.id
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
-  
-  # memo user_paramsにimage入れてね
 
 end
