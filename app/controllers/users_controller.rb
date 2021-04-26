72405cb78fb79331d4bb30e9f501683349c99d54
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-26 18:27:26 +0900

    update: ユーザーページ画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
   end
   
   def show
+    @users = User.find(params[:id])
+    # redirect_to picture_blogs_path, notice: "不正操作を記録しました。" unless current_user.id == @users.id
   end
 
   def edit
